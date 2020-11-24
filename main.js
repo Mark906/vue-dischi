4544ec24187cae24b4937d590151fd91701b8108
Marco <markoka1@outlook.it>
2020-11-24 20:53:05 +0100

    vue-dischi completo, inizializzazione bonus 1

---
+++ main.js
@@ -0,0 +1,34 @@
+var app = new Vue ({
+    el: "#wrapper",
+    data: {
+        music: [],
+        genere: [],
+        title: 'Elenco canzoni:',
+        selectedgenre: ''
+    },
+    mounted(){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((risposta) => {
+            console.log(risposta);
+            this.music = risposta.data.response;
+
+            this.music.forEach((item) => {
+                if(!this.genere.includes(item.genre)){
+                    this.genere.push(item.genre)
+                }
+            });
+
+
+            console.log(this.music);
+        });
+    },
+    methods: {
+        risposta(){
+            console.log(this.music);
+            console.log(this.genere);
+        }
+
+    }
+
+})
